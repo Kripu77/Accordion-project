5eee76d58c38bcfce47cc14d89ec884b782b1f71
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-10-05 15:04:15 +1100

    undo all the files

--- accordion-pro/src/Components/Accordion.js
+++ accordion-pro/src/Components/Accordion.js
@@ -1,40 +1,11 @@
-import React,{useState} from 'react'
-import data from './data.js';
-import { GrAddCircle, GrFormSubtract} from "react-icons/gr"
+import React from 'react'
 
 const Accordion = () => {
-  const [details, setDetails] = useState(data);
-  const[readmore, isReadMore] = useState (false);
-  const[click, setClick] = useState(true);
-
-    return (
-       <main >
-
-<h1 className="title"> Why you should learn JavaScript?</h1>
-<div className="bar"> </div>
-<article className="container">
-<h2> Some cool concpets of JavaScript</h2>
-<section >
-{details.map((value)=>{
-const {id, title, info } = value;
-
-return( <div key={id} className="item-container">
-    <div className="check">
-<h1 className="body-title"> {title}</h1>
-<p className="info"> {click? `${info}` : ""}</p>
-</div>
-
-<button className="icon" onClick={()=>setClick(
-    
-    !click
-    
-    )}> {click? <GrAddCircle/> : <GrFormSubtract/>}</button>
-</div>)
-
-})}
-</section>
-</article>
-       </main>)
+  return (
+    <div>
+      
+    </div>
+  )
 }
 
 export default Accordion
