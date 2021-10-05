ed81a4523b3f48c482a206964e217012a0ef425d
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-10-05 15:16:50 +1100

    udpate

--- accordion-pro/src/Components/Accordion.js
+++ accordion-pro/src/Components/Accordion.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React,{useState}from 'react'
+import data from "./data.js";
+import SingleAcco from './SingleAcco.js';
 
 const Accordion = () => {
+  const[questions, setQuestions] = useState(data);
+  console.log(questions);
   return (
-    <div>
-      
-    </div>
+    <main> 
+      <h1 className="title"> Why you should learn JavaScript?</h1>
+      <div className="bar"></div>
+     <section className="container">
+          <h2>Why you should learn JavaScript </h2>
+       {questions.map((value)=>{
+      const {id, info, title} =  value;
+         return (
+           <div key={id}>
+
+            
+           <SingleAcco id={id} info={info} title={title}/>
+             </div>
+         )
+       })}
+
+
+     </section>
+
+
+    
+    </main>
   )
 }
 
---
+++ accordion-pro/src/Components/SingleAcco.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { IconName } from "react-icons/gr";
+const SingleAcco = ({id, info, title}) => {
+    return (
+       <article>
+       
+           <div className="item-container">
+
+           </div>
+       </article>
+    )
+}
+
+export default SingleAcco
